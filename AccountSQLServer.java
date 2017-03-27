72c8aa62fa90e3e9d13ce8cffe809d9866d7ff95
Zhouweihua <nk_zhouwh@163.com>
2017-03-27 11:23:50 +0800

    SQL

---
+++ AccountSQLServer.java
@@ -0,0 +1,16 @@
+/*
+   编写程序读取科目表文件,全部插入或更新到SQLite数据库中。
+   (1)建立SQLServer数据库(Accounts),建立表Account（包括两个字段 id和name）
+   (2)从文件中读取科目表，将科目数据循环插入到SQLServer数据库中
+   (3)建立Account类，为从数据库中读取的每个科目构造Account对象，并在内存中建立科目树
+ 
+   
+   输入：固定科目.txt 动态科目.txt
+   输出：SQLServer数据库
+*/
+
+public class AccountSQLServer{
+	public static void main(String[] args){
+		
+	}
+}
